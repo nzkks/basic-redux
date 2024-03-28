384cbb5d7f224117c3df585bbac97fc9af31d21b
nzkks <kkshanthosh@gmail.com>
2024-03-29 06:22:46 +1300

    Add reducer function that accepts state and action as arguements and returns the nw state

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const BUY_CAKE = 'BUY_CAKE';
 
+// action object. The pupose of below function is to return an action object.
 function buyCake() {
   return {
     type: BUY_CAKE,
     info: 'First redux action'
   };
 }
+
+const initialState = {
+  numOfCakes: 10
+};
+
+// reducer function
+// (previousState, action) => newState
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1
+      };
+
+    default:
+      return state;
+  }
+};
