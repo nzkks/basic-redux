84779ce301464346dc75101d2d2479b89797520b
nzkks <kkshanthosh@gmail.com>
2024-03-29 06:59:09 +1300

    Add Redux store and add few action dispatches

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const BUY_CAKE = 'BUY_CAKE';
 
-// action object. The pupose of below function is to return an action object.
+// action creator object. The pupose of below function is to return an action object.
 function buyCake() {
   return {
     type: BUY_CAKE,
@@ -26,3 +29,20 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+// Redux Store holds application state
+const store = createStore(reducer);
+
+// Redux store allows access to state via getState()
+console.log('initial state', store.getState());
+
+// Redux store register listeners via subscribe(listener)
+const unsubscibe = store.subscribe(() => console.log('updated state', store.getState()));
+
+// Redux store allows state to be updated via dispatch(action)
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+// Redux store handles unregistering of listeners via the function returned by subscribe(listener)
+unsubscibe();
