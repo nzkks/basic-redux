b1b895c7fb882b10e4f0bcf830974579cefb0c49
nzkks <kkshanthosh@gmail.com>
2024-03-29 08:02:39 +1300

    Add middleware to the application to do something. Example logging with help of redux-logger

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
+
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM';
@@ -62,13 +66,13 @@ const rootReducer = combineReducers({
 });
 
 // Redux Store holds application state
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 // Redux store allows access to state via getState()
 console.log('initial state', store.getState());
 
 // Redux store register listeners via subscribe(listener)
-const unsubscibe = store.subscribe(() => console.log('updated state', store.getState()));
+const unsubscibe = store.subscribe(() => {});
 
 // Redux store allows state to be updated via dispatch(action)
 store.dispatch(buyCake());
