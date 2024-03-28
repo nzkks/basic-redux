3355b3ab3adb4a1519c92f3b425b6dd7acf12621
nzkks <kkshanthosh@gmail.com>
2024-03-29 07:50:18 +1300

    Multiple reducers: separation of concerrns

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require('redux');
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM';
@@ -19,14 +20,17 @@ function buyIceCream() {
   };
 }
 
-const initialState = {
-  numOfCakes: 10,
+const initialCakeState = {
+  numOfCakes: 10
+};
+
+const initialIceCreamState = {
   numOfIceCreams: 20
 };
 
 // reducer function
 // (previousState, action) => newState
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
@@ -34,6 +38,13 @@ const reducer = (state = initialState, action) => {
         numOfCakes: state.numOfCakes - 1
       };
 
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
     case BUY_ICECREAM:
       return {
         ...state,
@@ -45,8 +56,13 @@ const reducer = (state = initialState, action) => {
   }
 };
 
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer
+});
+
 // Redux Store holds application state
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 // Redux store allows access to state via getState()
 console.log('initial state', store.getState());
