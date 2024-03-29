37fb0e347de66ad743dbd090fc7192e40d4ce6a3
nzkks <kkshanthosh@gmail.com>
2024-03-30 09:06:29 +1300

    Add restock action

--- index.js
+++ index.js
@@ -8,20 +8,36 @@ const applyMiddleware = redux.applyMiddleware;
 const logger = reduxLogger.createLogger();
 
 const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
 const ICECREAM_ORDERED = 'ICECREAM_ORDERED';
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED';
 
 // action creator object. The pupose of below function is to return an action object.
 function orderCake() {
   return {
     type: CAKE_ORDERED,
-    quantity: 1
+    payload: 1
+  };
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty
   };
 }
 
 function orderIceCream() {
   return {
     type: ICECREAM_ORDERED,
-    quantity: 1
+    payload: 1
+  };
+}
+
+function restockIceCream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty
   };
 }
 
@@ -40,7 +56,12 @@ const cakeReducer = (state = initialCakeState, action) => {
     case CAKE_ORDERED:
       return {
         ...state,
-        numOfCakes: state.numOfCakes - action.quantity
+        numOfCakes: state.numOfCakes - 1
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload
       };
 
     default:
@@ -53,7 +74,12 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
     case ICECREAM_ORDERED:
       return {
         ...state,
-        numOfIceCreams: state.numOfIceCreams - action.quantity
+        numOfIceCreams: state.numOfIceCreams - 1
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload
       };
 
     default:
@@ -81,9 +107,11 @@ const unsubscribe = store.subscribe(() => {
 store.dispatch(orderCake());
 store.dispatch(orderCake());
 store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 
 store.dispatch(orderIceCream());
 store.dispatch(orderIceCream());
+store.dispatch(restockIceCream(2));
 
 // Redux store handles unregistering of listeners via the function returned by subscribe(listener)
 unsubscribe();
